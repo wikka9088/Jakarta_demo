f8531e5e490229dc75aa7e417ec5cdf89ed65425
V+ <wikka9088@gmail.com>
2018-05-21 23:01:23 -0400

    add jars

---
+++ First_web_application/src/com/demo/dao/jdbc/UserDAOJDBC.java
@@ -0,0 +1,26 @@
+package com.demo.dao.jdbc;
+
+import com.demo.dao.UserDAOInterface;
+import com.demo.entity.User;
+
+public class UserDAOJDBC implements UserDAOInterface {
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User getUserByNameAndPassword(String uname, String upwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateAvatar(String id, String avatar) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
